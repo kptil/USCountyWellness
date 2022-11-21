5f2f91c3c89531b4948d56d68e5b30751f9e60a8
kptil <ktiley@ufl.edu>
2022-11-20 21:58:27 -0500

    test db routes

--- UserInterface-HTML/table.js
+++ UserInterface-HTML/table.js
@@ -8,6 +8,14 @@ const _population = 'JWILLIAMS16.POPULATION';
 const _mother = 'WEISSB.MOTHER';
 const _hasRisk = 'WEISSB.MOTHERHASRISKFACTOR';
 const _motherRisk = 'WEISSB.MOTHER RISK';
+// created test tables to avoid key constraints on actual tables when entering dummy data
+const _county = 'KTILEY.COUNTYTEST';
+const _countHasEmployment = 'KTILEY.COUNTYHASEMPLOYMENTTEST'
+
+// TODO:
+// Adjust query 2 to incorporate at least state
+// Copy db_apis\counties.js to run query 2
+// Get results to show up under graph on Query 2 page
 
 
 module.exports = {
