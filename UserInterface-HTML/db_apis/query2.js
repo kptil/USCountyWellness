6e66a073fd01c91d4e8753d247da8949db7a9972
kptil <ktiley@ufl.edu>
2022-11-22 07:05:50 -0500

    actual query 2

--- UserInterface-HTML/db_apis/query2.js
+++ UserInterface-HTML/db_apis/query2.js
@@ -1,25 +1,17 @@
 const db = require('../services/database.js');
 const tb = require('../table.js')
 
-const baseQuery =`select * from ${tb.tables.countyTest}`;
-
-    //`SELECT * from ${tb.tables.countyTest} where (state_name = 'Florida');`;
-// Above gives an error that sql command is not properly ended,
-// without inner ; get poolAlias "default" not found in the connection pool cache error
-
-
-
-    /*`SELECT state_name, county_em_year, num / denom as rate
+const baseQuery = `SELECT state_name, county_em_year, num / denom as rate
                     from (
                         select state_name, county_em_year, sum(labor_force) as denom, sum(unemployed) as num
                         from (
                             (select coid, state_name from ${tb.tables.countyTest} where state_name = 'Florida')
                             natural join ${tb.tables.countyHasEmploymentTest}
                         )
-                    group by state_name, county_em_year
                     where state_name = 'Florida'
+                    group by state_name, county_em_year
                     )
-                    where (county_em_year >= 2000 and county_em_year <= 2005);`;*/
+                    where (county_em_year >= 2000 and county_em_year <= 2005)`;
 
 async function find(context) {
     let query = baseQuery;
